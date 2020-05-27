15ad918e94940903779b93686bcc90c9dc6f6da7
= <grzesiekkocik@gmail.com>
2020-05-28 00:41:18 +0200

    Going back to spaces instead of tabs in c#

--- main.sharpmake.cs
+++ main.sharpmake.cs
@@ -7,61 +7,61 @@ using Sharpmake;
 
 namespace MnemosyneSdk
 {
-	abstract public class BaseProject : Project
-	{
-		public BaseProject()
-		{
-			AddTargets(new Target(
-				Platform.win64,
-				DevEnv.vs2019,
-				Optimization.Debug | Optimization.Release,
-				OutputType.Dll
-			));
+    abstract public class BaseProject : Project
+    {
+        public BaseProject()
+        {
+            AddTargets(new Target(
+                Platform.win64,
+                DevEnv.vs2019,
+                Optimization.Debug | Optimization.Release,
+                OutputType.Dll
+            ));
 
-			SourceRootPath = @"[project.RootPath]\";
-		}
+            SourceRootPath = @"[project.RootPath]\";
+        }
 
-		[Configure]
-		public virtual void ConfigureAll(Project.Configuration conf, Target target)
-		{
-			conf.Name = @"[target.Optimization] [target.OutputType]";
-			conf.ProjectFileName = "[project.Name]_[target.DevEnv]_[target.Platform]";
-			conf.ProjectPath = @"[project.RootPath]\..\..\generated\";
-			conf.TargetPath = @"[project.RootPath]\..\..\bin\[target.Platform]_[target.OutputType]_[target.Optimization]\";
-			conf.Options.Add(Options.Vc.General.WindowsTargetPlatformVersion.Latest);
-		}
-	}
+        [Configure]
+        public virtual void ConfigureAll(Project.Configuration conf, Target target)
+        {
+            conf.Name = @"[target.Optimization] [target.OutputType]";
+            conf.ProjectFileName = "[project.Name]_[target.DevEnv]_[target.Platform]";
+            conf.ProjectPath = @"[project.RootPath]\..\..\generated\";
+            conf.TargetPath = @"[project.RootPath]\..\..\bin\[target.Platform]_[target.OutputType]_[target.Optimization]\";
+            conf.Options.Add(Options.Vc.General.WindowsTargetPlatformVersion.Latest);
+        }
+    }
 
-	[Generate]
-	public class MnemosyneSdk : Solution
-	{
-		public MnemosyneSdk()
-		{
-			Name = "MnemosyneSdk";
+    [Generate]
+    public class MnemosyneSdk : Solution
+    {
+        public MnemosyneSdk()
+        {
+            Name = "MnemosyneSdk";
 
-			AddTargets(new Target(
-				Platform.win64,
-				DevEnv.vs2019,
-				Optimization.Debug | Optimization.Release,
-				OutputType.Dll
-			));
-		}
+            AddTargets(new Target(
+                Platform.win64,
+                DevEnv.vs2019,
+                Optimization.Debug | Optimization.Release,
+                OutputType.Dll
+            ));
+        }
 
-		[Configure()]
-		public void ConfigureAll(Configuration conf, Target target)
-		{
-			conf.Name = @"[target.Optimization]_[target.OutputType]";
-			conf.SolutionFileName = "[solution.Name]_[target.DevEnv]_[target.Platform]";
-			conf.SolutionPath = @"[solution.SharpmakeCsPath]\generated\";
+        [Configure()]
+        public void ConfigureAll(Configuration conf, Target target)
+        {
+            conf.Name = @"[target.Optimization]_[target.OutputType]";
+            conf.SolutionFileName = "[solution.Name]_[target.DevEnv]_[target.Platform]";
+            conf.SolutionPath = @"[solution.SharpmakeCsPath]\generated\";
 
-			conf.AddProject<SdkUnitTests>(target);
-			conf.AddProject<Sdk>(target);
-		}
+            conf.AddProject<SdkUnitTests>(target);
+            conf.AddProject<Sdk>(target);
+        }
 
-		[Main]
-		public static void SharpmakeMain(Arguments arguments)
-		{
-			arguments.Generate<MnemosyneSdk>();
-		}
-	}
+        [Main]
+        public static void SharpmakeMain(Arguments arguments)
+        {
+            arguments.Generate<MnemosyneSdk>();
+        }
+    }
 }
--- src/sdk/sdk.sharpmake.cs
+++ src/sdk/sdk.sharpmake.cs
@@ -4,21 +4,21 @@ using Sharpmake;
 
 namespace MnemosyneSdk
 {
-	[Generate]
-	public class Sdk : BaseProject
-	{
-		public Sdk()
-		{
-			Name = "Sdk";
-		}
+    [Generate]
+    public class Sdk : BaseProject
+    {
+        public Sdk()
+        {
+            Name = "Sdk";
+        }
 
-		public override void ConfigureAll(Configuration conf, Target target)
-		{
-			base.ConfigureAll(conf, target);
+        public override void ConfigureAll(Configuration conf, Target target)
+        {
+            base.ConfigureAll(conf, target);
 
-			conf.IncludePaths.Add(@"[project.SharpmakeCsPath]\include");
-			conf.Defines.Add("MNEMOSYNE_SDK_API_EXPORT");
-			conf.Output = Configuration.OutputType.Dll;
-		}
-	}
+            conf.IncludePaths.Add(@"[project.SharpmakeCsPath]\include");
+            conf.Defines.Add("MNEMOSYNE_SDK_API_EXPORT");
+            conf.Output = Configuration.OutputType.Dll;
+        }
+    }
 }
